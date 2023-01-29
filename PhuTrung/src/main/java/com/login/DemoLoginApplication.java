fef3f40d117d6c98997203384e3fef37bbde6abf
trungprow <122389959+trungprow@users.noreply.github.com>
2023-01-29 14:11:50 +0700

    init project
    khởi tạo project cấu hình chạy đơn giản

---
+++ PhuTrung/src/main/java/com/login/DemoLoginApplication.java
@@ -0,0 +1,14 @@
+package com.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class DemoLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLoginApplication.class, args);
+	}
+
+}
---
+++ PhuTrung/src/main/java/com/login/domain/login.java
@@ -0,0 +1,6 @@
+package com.login.domain;
+
+
+public class login {
+
+}
